25cd468b03dc29d83c1762b90efe49e78a0bf1d3
larybino <binolaryssa@gmail.com>
2025-08-07 20:58:24 -0300

    feat: adiciona rota para tela de menu

--- lib/widgets/componentes/widget_drawer_menu.dart
+++ lib/widgets/componentes/widget_drawer_menu.dart
@@ -51,19 +51,31 @@ class _WidgetDrawerMenuState extends State<WidgetDrawerMenu> {
             currentAccountPicture: GestureDetector(
               onTap: () {
                 Navigator.pop(context);
-                Navigator.pushNamed(context, Rotas.usuario, arguments: _usuarioAtual);
+                Navigator.pushNamed(
+                  context,
+                  Rotas.usuario,
+                  arguments: _usuarioAtual,
+                );
               },
               child: CircleAvatar(
-                backgroundImage: (_usuarioAtual.fotoPerfil != null && _usuarioAtual.fotoPerfil!.isNotEmpty)
-                    ? NetworkImage(_usuarioAtual.fotoPerfil!)
-                    : null,
+                backgroundImage:
+                    (_usuarioAtual.fotoPerfil != null &&
+                            _usuarioAtual.fotoPerfil!.isNotEmpty)
+                        ? NetworkImage(_usuarioAtual.fotoPerfil!)
+                        : null,
                 backgroundColor: Colors.white,
-                child: (_usuarioAtual.fotoPerfil == null || _usuarioAtual.fotoPerfil!.isEmpty)
-                    ? Text(
-                        _usuarioAtual.nome?.substring(0, 1).toUpperCase() ?? 'U',
-                        style: const TextStyle(fontSize: 40.0, color: Color.fromARGB(255, 243, 33, 219)),
-                      )
-                    : null,
+                child:
+                    (_usuarioAtual.fotoPerfil == null ||
+                            _usuarioAtual.fotoPerfil!.isEmpty)
+                        ? Text(
+                          _usuarioAtual.nome?.substring(0, 1).toUpperCase() ??
+                              'U',
+                          style: const TextStyle(
+                            fontSize: 40.0,
+                            color: Color.fromARGB(255, 243, 33, 219),
+                          ),
+                        )
+                        : null,
               ),
             ),
             decoration: const BoxDecoration(
@@ -73,32 +85,67 @@ class _WidgetDrawerMenuState extends State<WidgetDrawerMenu> {
           _criarItemMenu(
             icone: Icons.checkroom,
             rotulo: 'Minhas Roupas',
-            aoPressionar: () => Navigator.pushNamed(context, Rotas.roupas, arguments: _usuarioAtual),
+            aoPressionar:
+                () => Navigator.pushNamed(
+                  context,
+                  Rotas.roupas,
+                  arguments: _usuarioAtual,
+                ),
           ),
           _criarItemMenu(
             icone: Icons.ice_skating,
             rotulo: 'Meus Sapatos',
-            aoPressionar: () => Navigator.pushNamed(context, Rotas.sapatos, arguments: _usuarioAtual),
+            aoPressionar:
+                () => Navigator.pushNamed(
+                  context,
+                  Rotas.sapatos,
+                  arguments: _usuarioAtual,
+                ),
           ),
           _criarItemMenu(
             icone: Icons.watch,
             rotulo: 'Meus Acessórios',
-            aoPressionar: () => Navigator.pushNamed(context, Rotas.acessorios, arguments: _usuarioAtual),
+            aoPressionar:
+                () => Navigator.pushNamed(
+                  context,
+                  Rotas.acessorios,
+                  arguments: _usuarioAtual,
+                ),
           ),
           _criarItemMenu(
             icone: Icons.style,
             rotulo: 'Meus Looks',
-            aoPressionar: () => Navigator.pushNamed(context, Rotas.looks, arguments: _usuarioAtual),
+            aoPressionar:
+                () => Navigator.pushNamed(
+                  context,
+                  Rotas.looks,
+                  arguments: _usuarioAtual,
+                ),
           ),
           _criarItemMenu(
             icone: Icons.event,
             rotulo: 'Meus Eventos',
-            aoPressionar: () => Navigator.pushNamed(context, Rotas.eventos, arguments: _usuarioAtual),
+            aoPressionar:
+                () => Navigator.pushNamed(
+                  context,
+                  Rotas.eventos,
+                  arguments: _usuarioAtual,
+                ),
           ),
           _criarItemMenu(
             icone: Icons.card_travel,
             rotulo: 'Minhas Malas',
-            aoPressionar: () => Navigator.pushNamed(context, Rotas.malas, arguments: _usuarioAtual),
+            aoPressionar:
+                () => Navigator.pushNamed(
+                  context,
+                  Rotas.malas,
+                  arguments: _usuarioAtual,
+                ),
+          ),
+          _criarItemMenu(
+            icone: Icons.home,
+            rotulo: 'Início',
+            aoPressionar: () => Navigator.pushNamed(context, Rotas.menu, arguments: _usuarioAtual),
           ),
           const Divider(),
           _criarItemMenu(
@@ -108,7 +155,8 @@ class _WidgetDrawerMenuState extends State<WidgetDrawerMenu> {
               final usuarioAtualizado = await Navigator.push<DTOUsuario>(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => WidgetEditarPerfil(usuario: _usuarioAtual),
+                  builder:
+                      (context) => WidgetEditarPerfil(usuario: _usuarioAtual),
                 ),
               );
               if (usuarioAtualizado != null) {
@@ -122,7 +170,11 @@ class _WidgetDrawerMenuState extends State<WidgetDrawerMenu> {
             icone: Icons.logout,
             rotulo: 'Sair',
             aoPressionar: () {
-              Navigator.pushNamedAndRemoveUntil(context, Rotas.login, (route) => false);
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                Rotas.login,
+                (route) => false,
+              );
             },
           ),
         ],
